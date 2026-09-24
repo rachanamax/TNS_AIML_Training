8e89cd287c94dd5e2e585242a724ef011d7febb3
rachanamax <bhangalerachana76@gmail.com>
2026-09-24 14:30:26 +0530

    Add files via upload

---
+++ modules2/app.py
@@ -0,0 +1,5 @@
+import mathutils as ma
+
+nums=list(map(int,input("enter the elements ").split()))
+final=ma.mathutils(nums)
+print(final)
---
+++ modules2/mathutils.py
@@ -0,0 +1,6 @@
+def mathutils(numbers):
+    return (set(numbers))
+if __name__=="__main__":
+    print("Testing: ", mathutils([]))
+
+    
